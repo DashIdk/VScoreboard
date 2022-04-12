437b765df053145abaa79ac18219ed39e2396119
DashIdk <81701356+DashIdk@users.noreply.github.com>
2022-04-12 10:08:20 -0400

    Allow for scoreboard style customization

--- ScoreboardMap.java
+++ ScoreboardMap.java
@@ -6,7 +6,21 @@ import com.google.common.collect.Maps;
 import java.util.Map;
 
 public class ScoreboardMap {
+    
+    public ScoreboardMap() {
+        this(15, true);
+    }
+    
+    public ScoreboardMap(int start, boolean descending) {
+        this.descending = descending;
+        this.current = start;
+    }
+    
+    private boolean descending;
+    private int current;
+    
     private Map<Integer, String> lines = Maps.newConcurrentMap();
+    
     public Map<Integer, String> getMap() {
         Map<Integer, String> map = Maps.newConcurrentMap();
         int k = lines.size() - 1;
@@ -15,7 +29,9 @@ public class ScoreboardMap {
     }
 
     public void add(String line) {
-        lines.put(lines.size(), line);
+        if(descending) current--;
+        else current++;
+        lines.put(current, line);
     }
 
     public boolean contains(String line) {
