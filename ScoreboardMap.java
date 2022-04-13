e33f41ee2e78e48d0fc8c168ae544f0c26f11bbb
DashIdk <81701356+DashIdk@users.noreply.github.com>
2022-04-12 21:43:13 -0400

    Forgot to remove some previous code

--- ScoreboardMap.java
+++ ScoreboardMap.java
@@ -6,26 +6,23 @@ import com.google.common.collect.Maps;
 import java.util.Map;
 
 public class ScoreboardMap {
-    
+
     public ScoreboardMap() {
         this(15, true);
     }
-    
+
     public ScoreboardMap(int start, boolean descending) {
         this.descending = descending;
         this.current = start + (descending ? 1 : -1);
     }
-    
+
     private boolean descending;
     private int current;
-    
+
     private Map<Integer, String> lines = Maps.newConcurrentMap();
-    
+
     public Map<Integer, String> getMap() {
-        Map<Integer, String> map = Maps.newConcurrentMap();
-        int k = lines.size() - 1;
-        for(int i : lines.keySet()) map.put(k--, lines.get(i));
-        return ImmutableMap.copyOf(map);
+        return ImmutableMap.copyOf(lines);
     }
 
     public void add(String line) {
